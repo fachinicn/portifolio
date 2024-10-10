f586b1900058d3ef3ebc5a0b7c0f8012f92c4248
netto <cfneto@edu.univali.br>
2024-10-10 10:37:05 -0300

    Atualizado o projeto Exame-E

--- src/components/ProjetoPainel.jsx
+++ src/components/ProjetoPainel.jsx
@@ -17,10 +17,9 @@ const projetosData = [
         icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg",
       },
     ],
-    description:
-      "Site que avalia resultado de exames laboratorias de sangue. O usuário assinala quais exames deseja verificar e insere os seus resultados, e com base em um banco de dados, é retornado para o usuário os valores de referência para os exames, indicando se está normal, acima do normal ou abaixo do normal.",
+    description: "EM DESENVOLVIMENTO",
     repoLink: "https://github.com/fachinicn/exam-e",
-    projectLink: "https://link-do-projeto1.com",
+    projectLink: "https://exam-e-ten.vercel.app",
   },
 ];
 
